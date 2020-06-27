89170ff6fbb0f9867684d333d5df70bfa2d4df07
Pradeep-selva <pradeepselvaraju@gmail.com>
2020-06-27 23:47:08 +0530

    db triggers for user image change and post deletion

--- connecsoc-functions/functions/index.js
+++ connecsoc-functions/functions/index.js
@@ -111,3 +111,71 @@ exports.onComment = functions
                 console.log(err);
             })
     })
+
+exports.onUserImageUpdate = functions
+    .region('asia-east2')
+    .firestore.document(`/users/{id}`)
+    .onUpdate(change => {
+        console.log(change.before.data().imgUrl);
+        console.log(change.after.data().imgUrl);
+
+        if (change.after.data().imgurl !== change.before.data().imgUrl) {
+            const batch = db.batch()
+            console.log("user image changed")
+
+            db.collection('posts')
+                .where('userHandle', '==', change.before.data().handle)
+                .get()
+                .then(data => {
+                    data.forEach(doc => {
+                        console.log(doc.id)
+                        const post = db.doc(`posts/${doc.id}`)
+                        batch.update(post, { userImg: change.after.data().imgUrl })
+                    })
+                    return batch.commit()
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        } else {
+            console.log("user image not changed")
+            return true;
+        }
+    })
+
+exports.onPostDelete = functions
+    .region('asia-east2')
+    .firestore.document(`/posts/{postId}`)
+    .onDelete((snapshot, context) => {
+        const postId = context.params.postId;
+        const batch = db.batch()
+
+        return db.collection('likes')
+            .where('postId', '==', postId)
+            .get()
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`likes/${doc.id}`))
+                })
+                return db.collection('comments')
+                    .where('postId', '==', postId)
+                    .get()
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`comments/${doc.id}`))
+                })
+                return db.collection('notifications')
+                    .where('postId', '==', postId)
+                    .get()
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`notifications/${doc.id}`))
+                })
+                return batch.commit()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
